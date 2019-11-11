b912f59b281da1110fff1035518fa6f9893100b8
Isabella Broch <5460278@RTS-365.dk>
2019-11-11 13:37:29 +0100

    Creating CRUD methods

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -16,52 +16,100 @@ const productFunctions = (() => ({
 
   readOne: (req, res) => {
     // get a single shit (read)
-    products.doc(req.params.id.toUpperCase()).get().then(doc => {
-      res.json(doc.data());
-    })
+    // For known doc name/structure...
+    products.doc(req.params.id.toUpperCase()).get().then(doc =>
+      res.json(doc.data())
+    )
+
+    // For unknown doc names
+    /* products.where("id", "==", parseInt(req.params.id)).limit(1).get().then(
+      docs => {
+        docs.forEach(
+          doc => res.json(doc.data())
+        )
+      }
+    ) */
   },
 
   deleteOne: (req, res) => {
     // delete shit (delete)
+
+    // For unknown doc names
+    /* products.where("id", "==", parseInt(req.params.id)).limit(1).get().then(
+      docs => docs.forEach(doc => {
+        doc.ref.delete()
+        res.status(204).end();
+      })
+    ).catch(err => res.status(500).json({
+      message: err
+    })); */
+
+    // For known doc names
+    products.doc(req.params.id.toUpperCase()).get()
+      .then(doc => {
+        doc.ref.delete()
+        res.status(204).end()
+      })
+      .catch(err => res.status(500).json({
+        message: err
+      }));
+
   },
 
   updateOne: (req, res) => {
-    // update shit (update / patch)
+    if (req.fields.id) {
+      req.fields.id = parseInt(req.fields.id);
+    }
+    if (req.fields.details) {
+      req.fields.details = JSON.parse(req.fields.details);
+    }
+    if (req.fields.description) {
+      req.fields.description = JSON.parse(req.fields.description);
+    }
+    if (req.fields.images) {
+      req.fields.images = JSON.parse(req.fields.images);
+    }
+
+    const data = {
+      ...req.fields
+    };
+
+    // For unknown doc names
+    /* products.where("id", "==", parseInt(req.params.id)).limit(1).get().then(
+      docs => docs.forEach(doc => {
+        doc.ref.update(data);
+        res.status(204).end();
+      })
+    ).catch(err => res.status(500).json({
+      message: err
+    })); */
+
+    // For known doc names
+    products.doc(req.params.id.toUpperCase()).get()
+      .then(doc => {
+        doc.ref.update(data);
+      })
+      .catch(err => res.status(500).json({
+        message: err
+      }));
+
+    products.doc(req.params.id.toUpperCase()).get().then(
+      doc => {
+        res.json(doc.data())
+      }
+    )
   },
 
   createOne: (req, res) => {
     // create shit (create / post)
+
+    req.fields.id = parseInt(req.fields.id);
+    req.fields.details = JSON.parse(req.fields.details);
+    req.fields.description = JSON.parse(req.fields.description);
+    req.fields.images = JSON.parse(req.fields.images);
+
     const data = {
       ...req.fields
-
-      // = {category, description, details, id, images, name}
-      // = category: req.fields.category
-      // = description: req.fields.description
-      // = details: req.fields.details
-      // ... osv...
-
-      // category: "cheese",
-      // description: ["words oo lala", "more words oooo"],
-      // details: {
-      //   country: "Denmark",
-      //   price: 500,
-      //   weight: 220
-      // },
-      // id: 8,
-      // images: [{
-      //     alt: "Primary image",
-      //     url: "cheese_01.jpg"
-      //   },
-      //   {
-      //     alt: "Secondary image",
-      //     url: "cheese_02.jpg"
-      //   },
-      //   {
-      //     alt: "Secondary image",
-      //     url: "cheese_03.jpg"
-      //   }
-      // ],
-      // name: "Mystery Cheese"
     };
 
     const key = `${data.category.substring(0,2).toUpperCase()}${`${data.id}`.padStart(3, "0")}`

73589d0ac654777726565287b64ec0f0a3d68541
Isabella Broch <5460278@RTS-365.dk>
2019-11-12 13:38:01 +0100

    Refactoring controller functions

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,128 +1,112 @@
 const products = require("../models/product.model");
+const { log } = require("../middleware/logger")
 
-const productFunctions = (() => ({
-  readAll: (req, res) => {
-    // get all shit (read)
-    products.get().then(docs => {
-      const data = {};
 
-      docs.forEach(doc => {
-        data[doc.id] = doc.data();
-      });
+function getDocsAsJSON(docs, res) {
+  if (docs.empty) {
+    res.status(404).send('No document(s) found');
+  } else {
+    const docsAsJSON = {};
 
-      res.json(data);
+    docs.forEach((doc) => {
+      docsAsJSON[doc.id] = doc.data()
     })
-  },
 
-  readOne: (req, res) => {
-    // get a single shit (read)
-    // For known doc name/structure...
-    products.doc(req.params.id.toUpperCase()).get().then(doc =>
-      res.json(doc.data())
-    )
-
-    // For unknown doc names
-    /* products.where("id", "==", parseInt(req.params.id)).limit(1).get().then(
-      docs => {
-        docs.forEach(
-          doc => res.json(doc.data())
-        )
-      }
-    ) */
-  },
-
-  deleteOne: (req, res) => {
-    // delete shit (delete)
-
-    // For unknown doc names
-    /* products.where("id", "==", parseInt(req.params.id)).limit(1).get().then(
-      docs => docs.forEach(doc => {
-        doc.ref.delete()
-        res.status(204).end();
-      })
-    ).catch(err => res.status(500).json({
-      message: err
-    })); */
-
-    // For known doc names
-    products.doc(req.params.id.toUpperCase()).get()
-      .then(doc => {
-        doc.ref.delete()
-        res.status(204).end()
-      })
-      .catch(err => res.status(500).json({
-        message: err
-      }));
-
-  },
-
-  updateOne: (req, res) => {
-    if (req.fields.id) {
-      req.fields.id = parseInt(req.fields.id);
-    }
-    if (req.fields.details) {
-      req.fields.details = JSON.parse(req.fields.details);
-    }
-    if (req.fields.description) {
-      req.fields.description = JSON.parse(req.fields.description);
-    }
-    if (req.fields.images) {
-      req.fields.images = JSON.parse(req.fields.images);
-    }
-
-    const data = {
-      ...req.fields
-    };
-
-    // For unknown doc names
-    /* products.where("id", "==", parseInt(req.params.id)).limit(1).get().then(
-      docs => docs.forEach(doc => {
-        doc.ref.update(data);
-        res.status(204).end();
-      })
-    ).catch(err => res.status(500).json({
-      message: err
-    })); */
-
-    // For known doc names
-    products.doc(req.params.id.toUpperCase()).get()
-      .then(doc => {
-        doc.ref.update(data);
-      })
-      .catch(err => res.status(500).json({
-        message: err
-      }));
-
-    products.doc(req.params.id.toUpperCase()).get().then(
-      doc => {
-        res.json(doc.data())
-      }
-    )
-  },
-
-  createOne: (req, res) => {
-    // create shit (create / post)
-
-    req.fields.id = parseInt(req.fields.id);
-    req.fields.details = JSON.parse(req.fields.details);
-    req.fields.description = JSON.parse(req.fields.description);
-    req.fields.images = JSON.parse(req.fields.images);
-
-    const data = {
-      ...req.fields
-    };
-
-    const key = `${data.category.substring(0,2).toUpperCase()}${`${data.id}`.padStart(3, "0")}`
-
-    products.doc(key).set(data)
-      .then(() => {
-        res.status(201)
-          .json({
-            [key]: data
-          });
-      })
-      .catch(error => res.json(error));
+    res.json(docsAsJSON);
   }
-}))();
+}
 
-module.exports = productFunctions;
+function correctFormTypes(form) {
+  if (form.id) {
+    form.id = parseInt(form.id);
+  }
+  if (form.details) {
+    form.details = JSON.parse(form.details);
+  }
+  if (form.description) {
+    form.description = JSON.parse(form.description);
+  }
+  if (form.images) {
+    form.images = JSON.parse(form.images);
+  }
+
+  return {
+    fields: form,
+    key: getKey(form.category, form.id)
+  }
+}
+
+function getKey(category, id) {
+  return `${category.substring(0,2).toUpperCase()}${`${id}`.padStart(3, "0")}`
+}
+
+
+const productsCrud = (() =>
+  ({
+    readAll: async (req, res) => {
+      try {
+        const allDocs = await products.get();
+        getDocsAsJSON(allDocs, res)
+      } catch (error) {
+        res.status(500).end();
+        log.error(error)
+      }
+    },
+
+    readOne: async (req, res) => {
+      try {
+        console.log(req.params);
+        const oneDoc = await products.where("id", "==", parseInt(req.params.id)).limit(1).get();
+        getDocsAsJSON(oneDoc, res)
+      } catch (error) {
+        res.status(500).end();
+        log.error(error)
+      }
+    },
+
+    deleteOne: async (req, res) => {
+      try {
+        const oneDoc = await products.where("id", "==", parseInt(req.params.id)).limit(1).get();
+        oneDoc.forEach((doc) => {
+          doc.ref.delete();
+          res.status(204).end();
+        });
+      } catch (error) {
+        res.status(500).end();
+        log.error(error)
+      }
+    },
+
+    updateOne: async (req, res) => {
+      try {
+        const oneDoc = await products.where("id", "==", parseInt(req.params.id)).limit(1).get();
+        oneDoc.forEach((doc) => {
+          doc.ref.update(correctFormTypes(req).fields);
+          res.status(204).end();
+        });
+      } catch (error) {
+        res.status(500).end();
+        log.error(error)
+      }
+    },
+
+    createOne: async (req, res) => {
+      try {
+        if (Object.entries(req.fields).length < 6) {
+          res.status(404).send("Missing fields").end();
+        } else {
+          const data = correctFormTypes(req.fields);
+          products.doc(data.key).set(data.fields);
+          res.status(201).send(`Creating document with key ${data.key}`).end();
+        }
+      } catch (error) {
+        res.status(500).end();
+        log.error(error)
+      }
+    }
+  })
+
+)();
+
+
+module.exports = productsCrud;
